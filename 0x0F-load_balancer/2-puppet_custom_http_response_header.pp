78394dd811e23c1a405287142f2a3bcefbe81c5b
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-04 01:13:26 +0300

    [Chore]: Add advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,30 @@
-# Puppet manifest to configure a webserver
-# to install nginx and add custom http headers
+# Puppet manifest to setup up an ubuntu server
+# With nginx, custom http headers
 
-exec { 'Updated System':
-        command  => '/usr/bin/apt-get update',
-        provider => 'shell'
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['Updated System']
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
-  content => 'Hello World!',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '7624'
+	content => 'Hello World!'
 }
 
 exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-exec {'Custom Header':
-  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+exec {'Custom HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
 service {'nginx':
-  ensure  => running,
-  require => Package['nginx']
+	ensure => running,
+	require => Package['nginx']
 }
