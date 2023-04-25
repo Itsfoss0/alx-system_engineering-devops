093799f7ab78d085dd891c5a4c5591d3a2e86687
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-25 09:40:30 +0300

    [Chore]: Add task two

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,53 +1,39 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""A simple module to make API Calls
+To a mockup API server and return the
+Responses. Then print them out to standard output
+Usage: ./0-gather-data_from_an_API <ID>
+Where <ID> is the employee ID for whom we want to list
+The tasks"""
 
-"""A module to make API calls to
-a mockup API server and print the responses to standard output.
-Usage:
-./0-gather-data_from_an_API.py <ID>
-Where <ID> is the employee ID for whom we want to list the tasks.
-"""
+from requests import get
+from sys import argv
 
-import pathlib
-import requests
-import sys
-
-
-API_BASE_URL = "https://jsonplaceholder.typicode.com"
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "User-Agent": "Thing Gecko/20100101 Firefox/102.0"
+}
+base_url = "https://jsonplaceholder.typicode.com/users/"
 
 
-def get_tasks(user_id: str) -> None:
+def save_task_status_to_csv(user_id: str) -> None:
     """
-    Get the task status for a certain user and save the results to a CSV file.
+    Get the task status for a certain user and save 'em
     Args:
-        user_id (str): The user id of the user.
+        user_id (str): The user id of the user
     """
-    try:
-        response = requests.get(f"{API_BASE_URL}/users/{user_id}")
-        response.raise_for_status()
-        emp_name = response.json().get("name")
-
-        response = requests.get(f"{API_BASE_URL}/users/{user_id}/todos/",
-                                headers={"User-Agent": "Firefox/102.0"})
-        response.raise_for_status()
-
-        tasks = response.json()
-        filename = pathlib.Path(f"{user_id}.csv")
-
-        with filename.open("w", encoding="utf-8") as csv_file:
-            for task in tasks:
-                line_content = f'"{user_id}",\
-                "{emp_name}", "{task["completed"]}", "{task["title"]}"'
-                csv_file.write(line_content + "\n")
-
-    except Exception as e:
-        print(f"Error: {e}", file=sys.stderr)
-        sys.exit(1)
-
+    # lets first get the name of Employee
+    emp_name = get("{}{}".format(base_url, user_id)).json().get("name")
+    full_url = "{}{}/todos/".format(base_url, user_id)
+    response = get(full_url, headers=headers).json()
+    # save the tasks that belong to this user to a csv file
+    file_name = "{}.csv".format(user_id)
+    with open(file_name, "w", encoding="utf-8") as csv_file:
+        for resp in response:
+            csv_file.write('"{}","{}","{}","{}"\n'\
+                .format(resp.get("userId"), emp_name, resp.get("completed"),\
+                    resp.get("title")))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <ID>", file=sys.stderr)
-        sys.exit(1)
-
-    user_id = sys.argv[1]
-    get_tasks(user_id)
+    save_task_status_to_csv(argv[1])
