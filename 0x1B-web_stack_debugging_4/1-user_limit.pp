500ee09387f0e60050f57d0ff3e1a0cf25732ded
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-18 19:05:13 +0300

    [Fix]: Allow holberton user to open files

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,12 +3,12 @@
 # open files without any errors
 
 exec { 'changing-security-limits':
-  command  => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path     => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
 # increament soft file limits holberton
 exec { 'increament-security-limits-for-holberton-user':
-  command  => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path     => '/usr/local/bin/:/bin/',
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/',
 }
