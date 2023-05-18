6cf78188ee4b70d4e2c8ccbad67265752ee8954e
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-18 19:02:47 +0300

    [Chore]: change OS configurations

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/env puppet
+# Allow the user `holberton`  to login and
+# open files without any errors
+
+exec { 'changing-security-limits':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  provider => 'shell',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# increament soft file limits holberton
+exec { 'increament-security-limits-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/',
+  provider => 'shell'
+}
