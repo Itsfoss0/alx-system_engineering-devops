8e40b8563d4cd1f4fa0dba4f0c7b216fc14a39b4
Ebenezer <akhonyaebenezer@gmail.com>
2023-05-18 19:33:12 +0300

    [Chore]: Add task two

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,13 +2,15 @@
 # Allow the user `holberton`  to login and
 # open files without any errors
 
-exec { 'changing-security-limits':
+exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
+  provider => 'shell'
 }
 
-# increament soft file limits holberton
-exec { 'increament-security-limits-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+# make some tweaks on the soft file limit
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
+  provider => 'shell'
 }
